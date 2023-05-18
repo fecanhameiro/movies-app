937c6ebcec4804237411df8432eafa82d1a305a6
Felipe C <fecanhameiro@gmail.com>
2023-05-18 18:08:56 +0200

    up the first settings

---
+++ Movies App - UIKit/MoviesAppUIKit/MoviesAppUIKit/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MoviesAppUIKit
+//
+//  Created by Felipe C Canhameiro on 18/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
